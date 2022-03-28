557828a58defe494ca65f4de8bb86894f1040f2e
Thore Haupt <thore.haupt@t-online.de>
2022-03-28 18:47:36 +0200

    s

--- README.md
+++
@@ -1,2 +0,0 @@
-# FactorioBallisticMissiles
-A mod for factorio
---
+++ prototypes/launch-remote.lua
@@ -0,0 +1,10 @@
+data:extend({{
+    type = "recipe",
+    name = "Missile Remote",
+    energy_required = 0.5,
+    enabled = true,
+    category = "crafting",
+    ingredients = {{"Rocket controll unit", 10}, {"Radar", 2}, {"processing unit", 100}},
+    result = "wood",
+    result_count = 1
+}})
